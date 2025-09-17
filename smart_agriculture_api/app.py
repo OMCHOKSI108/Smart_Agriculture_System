9d54a84e6625df73c871eba98c1ff11840650b0e
Hari Patel <patelhariv18@gmail.com>
2025-09-17 20:50:25 +0530

    Modified flask app

--- smart_agriculture_api/app.py
+++ smart_agriculture_api/app.py
@@ -209,4 +209,5 @@ def predict_plant_disease():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
